0bdfc0a6d9d924081c774b305a3116fac22ed2f4
Fredrik Larsson <fredrik.larsson@medieinstitutet.se>
2020-06-01 09:21:06 +0200

    Förtydligade med en kommentar

--- wcms19-relatedposts.php
+++ wcms19-relatedposts.php
@@ -70,6 +70,7 @@ add_action('init', 'wrp_init');
 
 function wrp_the_content($content)  {
     if (is_single()) {
+        // If there is NO shortcode called "related-posts" in $content, add it to end of $content
         if (!has_shortcode($content, 'related-posts')) {
             $related_posts = wrp_get_related_posts();
             $content = $content . $related_posts;
